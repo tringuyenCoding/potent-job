d5d070c94679485ab3c664d40e1865ccee6ac8f3
tringuyen@motive <95064327+tringuyenCoding@users.noreply.github.com>
2024-11-19 12:04:21 +0700

    Implement api and authentication for user profile

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,19 +1,20 @@
-import {User} from "../models/user.model.js";
-import bcrypt from "bcrypt";
+import { User } from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
   try {
-    const {fullname, email, phoneNumber, password, role} = req.body;
+    const { fullname, email, phoneNumber, password, role } = req.body;
 
-    if(!(email && password && fullname && phoneNumber && role)){
+    if (!(email && password && fullname && phoneNumber && role)) {
       return res.status(400).json({
         message: "Something is missing",
         success: false,
       });
     }
 
-    const user = await User.findOne({email});
-    if(user){
+    const user = await User.findOne({ email });
+    if (user) {
       return res.status(400).json({
         message: "User already exists",
         success: false,
@@ -21,7 +22,7 @@ export const register = async (req, res) => {
     }
 
     const encryptedPassword = await bcrypt.hash(password, 10);
-    
+
     await User.create({
       fullname,
       email,
@@ -35,25 +36,24 @@ export const register = async (req, res) => {
       success: true,
     });
   } catch (error) {
-    console.log(error); 
+    console.log(error);
   }
 };
 
-
 export const login = async (req, res) => {
   try {
-    const {email,password, role} = req.body
+    const { email, password, role } = req.body;
 
-    if(!(email && password && role)){
+    if (!(email && password && role)) {
       return res.status(400).json({
         message: "Something is missing",
         success: false,
       });
     }
 
-    const user = await User.findOne({email});
+    let user = await User.findOne({ email });
 
-    if(!user){
+    if (!user) {
       return res.status(400).json({
         message: "Incorrect email or password",
         success: false,
@@ -61,18 +61,27 @@ export const login = async (req, res) => {
     }
 
     const isPasswordMatch = await bcrypt.compare(password, user.password);
-    if(!isPasswordMatch){
+    if (!isPasswordMatch) {
       return res.status(400).json({
         message: "Incorrect email or password",
         success: false,
       });
     }
 
-    const tokenData ={
-      userId: user._id,
+    if (role !== user.role) {
+      return res.status(400).json({
+        message: "Account doesn't exist with current role",
+        success: false,
+      });
     }
 
-    const token = await jwt.sign(tokenData,process.env.SECRET_KEY, {expiresIn: "1d"});
+    const tokenData = {
+      userId: user._id,
+    };
+
+    const token = await jwt.sign(tokenData, process.env.SECRET_KEY, {
+      expiresIn: "1d",
+    });
 
     user = {
       _id: user._id,
@@ -81,22 +90,75 @@ export const login = async (req, res) => {
       phoneNumber: user.phoneNumber,
       role: user.role,
       profile: user.profile,
-    }
+    };
 
-    return res.status(200).cookie("token", token, {maxAge: 1*24*60*60*1000, httpsOnly: true, sameSite: 'strict'}).json({
-      message: "Login Successful",
-      user,
-      success: true,
-    });
-
+    return res
+      .status(200)
+      .cookie("token", token, {
+        maxAge: 1 * 24 * 60 * 60 * 1000,
+        httpsOnly: true,
+        sameSite: "strict",
+      })
+      .json({
+        message: "Login Successful",
+        user,
+        success: true,
+      });
   } catch (error) {
     console.log(error);
   }
 };
 
 export const logout = async (req, res) => {
-  return res.status(200).cookie("token","",{maxAge:0}).json({
+  return res.status(200).cookie("token", "", { maxAge: 0 }).json({
     message: "Logout Successful",
     success: true,
   });
+};
+
+export const updateProfile = async (req, res) => {
+  try {
+    const { fullname, email, phoneNumber, bio, skills } = req.body;
+    const file = req.file;
+
+    let skillsArray;
+    if (skills) skillsArray = skills.split(",");
+
+    const userId = req.id; // middeware authentication
+    let user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(400).json({
+        message: "User not found",
+        success: false,
+      });
+    }
+
+    // Update user profile
+    if (fullname) user.fullname = fullname;
+    if (email) user.email = email;
+    if (phoneNumber) user.phoneNumber = phoneNumber;
+    if (bio) user.profile.bio = bio;
+    if (skills) user.profile.skills = skillsArray;
+
+    // resume comes later here
+
+    await user.save();
+    user = {
+      _id: user._id,
+      fullname: user.fullname,
+      email: user.email,
+      phoneNumber: user.phoneNumber,
+      role: user.role,
+      profile: user.profile,
+    };
+
+    return res.status(200).json({
+      message: "Profile updated successfully",
+      user,
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './utils/db.js';
+import userRouter from './routes/user.route.js';
+
 dotenv.config({});
 
 const app = express();
@@ -18,8 +20,13 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-
 const PORT = process.env.PORT || 3000;
+
+// API's
+
+app.use('/api/v1/user', userRouter);
+
+
 app.listen(PORT, () => {
   connectDB();
   console.log(`Server is running on port ${PORT}`);
