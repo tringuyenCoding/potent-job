fd557210f37a760cd632d3ab76e1a9fa5ab04c2d
tringuyen@motive <95064327+tringuyenCoding@users.noreply.github.com>
2024-11-25 16:21:08 +0700

    [FE]: implement the hero section in hone page

---
+++ frontend/src/components/HeroSection.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Button } from './ui/button'
+import { Search } from 'lucide-react'
+
+const HeroSection = () => {
+  return (
+    <div className='text-center'>
+      <div className='flex flex-col gap-5 my-10'>
+        <span className='mx-auto px-4 py-2 rounded-full bg-gray-100 text-[#F83002] font-medium'>Potential Jobs For You</span>
+        <h1 className='text-5xl font-bold'>Search, Apply & <br />Get Your <span className='text-[#6a38c2]'>Dream Jobs</span></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic quaerat ab earum fuga saepe!</p>
+        <div className='flex w-[40%] shadow-lg border border-gray-200 pl-3 rounded-full items-center gap-4 mx-auto'>
+          <input 
+            type="text"
+            placeholder='Find your potential jobs'
+            className='outline-none border-none w-full' />
+
+          <Button className='rounded-r-full bg-[#6a38c2]'>
+            <Search className="h-5 w-5"/>
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HeroSection
