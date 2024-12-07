1e74a49426c4dc94bfc344260b3d254c882f2ce0
tringuyen@motive <95064327+tringuyenCoding@users.noreply.github.com>
2024-12-07 14:57:28 +0700

    [FE+BE] update company info

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -3,7 +3,6 @@ import authSlice from "./authSlice";
 import jobSlice from "./jobSlice";
 import companySlice from "./companySlice";
 import {
-  persistStore,
   persistReducer,
   FLUSH,
   REHYDRATE,
