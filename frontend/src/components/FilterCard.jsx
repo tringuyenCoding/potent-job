9d5fb15408152e47be0eee95c5f21fad45c2e1c9
tringuyen@motive <95064327+tringuyenCoding@users.noreply.github.com>
2024-12-27 09:23:47 +0700

    [FE]: filter jobs based on different criteria

--- frontend/src/components/FilterCard.jsx
+++ frontend/src/components/FilterCard.jsx
@@ -1,5 +1,6 @@
 import { RadioGroup, RadioGroupItem } from "./ui/radio-group";
 import { Label } from "./ui/label";
+import { useEffect, useState } from "react";
 
 const filterData = [
   {
@@ -17,19 +18,30 @@ const filterData = [
 ];
 
 const FilterCard = () => {
+  const [selectedValue, setSelectedValue] = useState("");
+
+  const changeEvent = (value) => {
+    setSelectedValue(value);
+  };
+
+  useEffect(() => {
+    console.log(selectedValue);
+  }, [selectedValue]);
+
   return (
     <div className="w-full bg-white p-3 rounded-md">
       <h1 className="font-bold text-lg">Filter Jobs</h1>
       <hr className="mt-3" />
-      <RadioGroup>
+      <RadioGroup value={selectedValue} onValueChange={changeEvent}>
         {filterData.map((data, index) => (
-          <div>
+          <div key={index}>
             <h1 className="font-bold text-lg">{data.filterType}</h1>
-            {data.details.map((detail, index) => {
+            {data.details.map((detail, idx) => {
+              const itemid = `id${detail}-${idx}`;
               return (
-                <div className="flex items-center space-x-2 my-2">
+                <div key={idx} className="flex items-center space-x-2 my-2">
                   <RadioGroupItem value={detail} />
-                  <Label>{detail}</Label>
+                  <Label htmlFor={itemid}>{detail}</Label>
                 </div>
               );
             })}
